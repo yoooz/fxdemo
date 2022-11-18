8ed91f98e9f0abdb1e1b0673b7191892122fe113
yoooz <oor.9@outlook.com>
2022-11-18 14:10:02 +0900

    Register another handler

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net"
 	"net/http"
@@ -18,8 +19,19 @@ func main() {
 		}),
 		fx.Provide(
 			NewHTTPServer,
-			fx.Annotate(NewEchoHandler, fx.As(new(Route))),
-			NewServeMux,
+			fx.Annotate(
+				NewEchoHandler,
+				fx.As(new(Route)),
+				fx.ResultTags(`name:"echo"`)),
+			fx.Annotate(
+				NewHelloHandler,
+				fx.As(new(Route)),
+				fx.ResultTags(`name:"hello"`),
+			),
+			fx.Annotate(
+				NewServeMux,
+				fx.ParamTags(`name:"echo"`, `name:"hello"`),
+			),
 			zap.NewExample,
 		),
 		fx.Invoke(func(*http.Server) {}),
@@ -59,9 +71,10 @@ func (h *EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func NewServeMux(route Route) *http.ServeMux {
+func NewServeMux(route1, route2 Route) *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.Handle(route.Pattern(), route)
+	mux.Handle(route1.Pattern(), route1)
+	mux.Handle(route2.Pattern(), route2)
 	return mux
 }
 
@@ -72,4 +85,31 @@ type Route interface {
 
 func (*EchoHandler) Pattern() string {
 	return "/echo"
+}
+
+type HelloHandler struct {
+	log *zap.Logger
+}
+
+func NewHelloHandler(log *zap.Logger) *HelloHandler {
+	return &HelloHandler{log: log}
+}
+
+func (*HelloHandler) Pattern() string {
+	return "/hello"
+}
+
+func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		h.log.Error("Failed to read request", zap.Error(err))
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := fmt.Fprintf(w, "Hello, %s\n", body); err != nil {
+		h.log.Error("Failed to write response", zap.Error(err))
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
 }
